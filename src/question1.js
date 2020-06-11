077db8664a4d58525abe392c092d852b069acb90
kako-jun <2748661@gmail.com>
2020-06-12 04:42:23 +0900

    question1

---
+++ src/question1.js
@@ -0,0 +1,9 @@
+for (var num = 0; num >= 0 && num <= 100; num++ && num !== 0) {
+  if (num % 4 === 0 && num % 8 !== 0) {
+    console.log('Fizz');
+  } else if (num % 8 === 0 && num % 4 === 0 && num !== 0) {
+    console.log('FizzBuzz');
+  } else {
+    console.log(num);
+  }
+}
