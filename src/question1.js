7d0500b53c0e7b0d99a8c23947a0a1f3d5b74a69
kako-jun <2748661@gmail.com>
2020-06-12 10:15:18 +0900

    question1(修正)

--- src/question1.js
+++ src/question1.js
@@ -1,4 +1,4 @@
-for (var num = 0; num >= 0 && num <= 100; num++ && num !== 0) {
+for (let num = 0; num >= 0 && num <= 100; num++ && num !== 0) {
   if (num % 4 === 0 && num % 8 !== 0) {
     console.log('Fizz');
   } else if (num % 8 === 0 && num % 4 === 0 && num !== 0) {
